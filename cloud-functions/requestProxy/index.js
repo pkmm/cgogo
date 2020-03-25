91f9246d91847d1b71f8c28f8321de4161aa92e4
pkmm <robotgg@126.com>
2020-03-25 11:38:27 +0800

    b

--- cloud-functions/requestProxy/index.js
+++ cloud-functions/requestProxy/index.js
@@ -38,17 +38,17 @@ function modifyHost(options) {
  * @example
  * 参数options
  * const options = {
-      url: 'https://api.github.com/repos/request/request',
-      headers: {
-         'User-Agent': 'request'
-      },
-      method: 'get', // default is get
-      json: true, //  json 设置为true，要保证body是可以json序列化的，同时返回的body应该是json数据
-      body: {}, // 一个可以json序列化的数据
-      encoding: // 用于设置返回的请求的数据格式 如果是null，返回是buffer（也就是二进制数据），任何的其他值都会把返回数据编码成string
-      qs: {}, // 一个object表示query的参数，
-      //useQuerystring：true, // 格式化qs为string的形式
-    };
+ *     url: 'https://api.github.com/repos/request/request',
+ *     headers: {
+ *        'User-Agent': 'request'
+ *     },
+ *     method: 'get', // default is get
+ *     json: true, //  json 设置为true，要保证body是可以json序列化的，同时返回的body应该是json数据
+ *     body: {}, // 一个可以json序列化的数据
+ *     encoding: // 用于设置返回的请求的数据格式 如果是null，返回是buffer（也就是二进制数据），任何的其他值都会把返回数据编码成string
+ *     qs: {}, // 一个object表示query的参数，
+ *     //useQuerystring：true, // 格式化qs为string的形式
+ *   };
  */
 exports.main = async (evt, ctx) => {
   return new Promise((resolve, reject) => {
